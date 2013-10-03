dda795ac0126a46c2af87262ff0c2f8d35941694
Kash Shamim <kshamim@fusepowered.com>
2013-10-03 15:24:49 -0400

    fix for occasional crash on iOS due to unsynchronized dictionary

--- Assets/Plugins/iOS/FuseAPI.h
+++ Assets/Plugins/iOS/FuseAPI.h
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright 2009-2012 Fuse Powered, Inc.
+//  Copyright 2009-2013 Fuse Powered, Inc.
 // 
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
